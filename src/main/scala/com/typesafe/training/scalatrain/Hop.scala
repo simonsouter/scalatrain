f7dedd138b9f408a57902b82fd0bcb137fcddf56
Charles Rice <charles.rice92@gmail.com>
2015-05-14 09:45:28 +0100

    hop from def to val

--- src/main/scala/com/typesafe/training/scalatrain/Hop.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Hop.scala
@@ -8,13 +8,9 @@ case class Hop(from: Station, to: Station, train: Train) {
   require(train.stations.contains(from), s"$train train must pass the $from station!")
   require(train.stations.contains(to), s"$train train must pass the $to station!")
 
-  def departureTime = {
-    train.timeAt(from).get
-  }
+  val departureTime: Time = train.timeAt(from).get
 
-  def arrivalTime = {
-    train.timeAt(to).get
-  }
+  val arrivalTime: Time = train.timeAt(to).get
 
 //  override def compare(that: Hop): Int = {
 //    val thisTime = (arrivalTime, departureTime)
