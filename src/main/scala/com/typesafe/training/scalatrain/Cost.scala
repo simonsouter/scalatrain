d9ec8ed3374e8a520df952eaf60f682bece13af0
Charles Rice <charles.rice92@gmail.com>
2015-05-15 11:41:51 +0100

    currency functionality

--- src/main/scala/com/typesafe/training/scalatrain/Cost.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Cost.scala
@@ -6,10 +6,16 @@ package com.typesafe.training.scalatrain
 
 import java.math.RoundingMode
 
-import org.joda.money.{BigMoney, Money}
+import org.joda.money.{CurrencyUnit, Money}
 import org.joda.money.CurrencyUnit._
 
+import scala.concurrent.Await
+import scala.concurrent.duration.FiniteDuration
+
 object Cost {
+
+  lazy val fixerApi = new FixerIO
+
   /**
    * Given length of a Hop, generate the cost
    * @param minutes Minutes from departure time to arrival time, as an Int
@@ -23,5 +29,10 @@ object Cost {
       case x if x > 120 => Money.parse("GBP 9.99")
       case default => Money.parse("GBP 1.00")
     }) multipliedBy(modifier, RoundingMode.DOWN)
+  }
+
+  def convertCurrency(value: Money, targetCurrency: CurrencyUnit): Money = {
+    val rate = Await.result(fixerApi.getExchange(targetCurrency.toString), FiniteDuration(5, "seconds"))
+    value convertedTo(targetCurrency, new java.math.BigDecimal(rate), RoundingMode.DOWN)
   }
 }
--- src/test/scala/com/typesafe/training/scalatrain/CostSpec.scala
+++ src/test/scala/com/typesafe/training/scalatrain/CostSpec.scala
@@ -12,6 +12,9 @@ import org.joda.money.Money
 import org.joda.money.CurrencyUnit._
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.concurrent.Await
+import scala.concurrent.duration.FiniteDuration
+
 class CostSpec extends WordSpec with Matchers {
 
   "Cost class" should {
@@ -19,6 +22,13 @@ class CostSpec extends WordSpec with Matchers {
       Cost.generateCost(59, 1.0) shouldEqual Money.of(GBP, 2.50)
       Cost.generateCost(69, 1.2) shouldEqual Money.of(GBP, 3.50).multipliedBy(1.2, RoundingMode.DOWN)
       Cost.generateCost(199, 0.5) shouldEqual Money.of(GBP, 9.99).multipliedBy(0.5, RoundingMode.DOWN)
+    }
+    "convert currencies" in {
+      val fixerApi = new FixerIO
+      val oneGbp = Money.of(GBP, 1.00)
+      val gbp2usd = Await.result(fixerApi.getExchange(USD.toString), FiniteDuration(5, "seconds"))
+
+      Cost.convertCurrency(oneGbp, USD) shouldEqual oneGbp.convertedTo(USD, new java.math.BigDecimal(gbp2usd), RoundingMode.DOWN)
     }
   }
 
